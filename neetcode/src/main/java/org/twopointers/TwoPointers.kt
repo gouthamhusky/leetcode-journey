7e591b89d12037ffce880b9bf0e6c278f96eed36
Goutham K <kanahasabai.g@northeastern.edu>
2023-09-08 18:03:23 -0400

    chore: two pointer problems

---
+++ neetcode/src/main/java/org/twopointers/TwoPointers.kt
@@ -0,0 +1,63 @@
+package org.twopointers
+
+import java.lang.StringBuilder
+
+
+fun main() {
+    threeSum(intArrayOf(0, 0, 0, 0))
+}
+
+// https://leetcode.com/problems/valid-palindrome/
+fun isPalindrome(s: String): Boolean {
+    val parsedString = StringBuilder()
+    for (c in s.toCharArray()){
+        when {
+            c.isUpperCase() -> parsedString.append(c.toLowerCase())
+            !c.isLetterOrDigit() -> continue
+            else -> parsedString.append(c)
+        }
+    }
+    var i = 0
+    var j = parsedString.length - 1
+    while (i < j){
+        if (parsedString[i] != parsedString[j])
+            return false
+        i++
+        j--
+    }
+    return true
+}
+
+fun twoSum(numbers: IntArray, target: Int): IntArray {
+    var left = 0
+    var right = numbers.size - 1
+    while (left < right){
+        if (numbers[left] + numbers[right] == target)
+            return intArrayOf(left + 1, right + 1)
+        when{
+            numbers[left] + numbers[right] == target -> return intArrayOf(left + 1, right + 1)
+            numbers[left] + numbers[right] > target -> right--
+            else -> left++
+        }
+    }
+    return intArrayOf()
+}
+
+// https://leetcode.com/problems/3sum/
+fun threeSum(nums: IntArray): List<List<Int>> {
+    nums.sort()
+    val solution = mutableSetOf<List<Int>>()
+    for (i in nums.indices) {
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue
+        val current = nums[i]
+        val map = mutableMapOf<Int, Int>()
+        for (j in i + 1 until nums.size) {
+            if (map.containsKey(0 - (current + nums[j]))){
+                solution.add(listOf(current, nums[j], map.get(0 - (current + nums[j]))!!))
+            }
+            map.put(nums[j], nums[j])
+        }
+    }
+    return solution.toList()
+}
